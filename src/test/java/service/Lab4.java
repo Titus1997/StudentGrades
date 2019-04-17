b2e55a082d2bd1be73e6a000d4fede91c978939f
Titus1997 <trifon.titus@yahoo.com>
2019-04-17 12:55:14 +0300

    big bang integration

---
+++ src/test/java/service/Lab4.java
@@ -0,0 +1,56 @@
+package service;
+
+import domain.Nota;
+import domain.Student;
+import domain.Tema;
+import junit.framework.TestCase;
+import repository.NotaXMLRepository;
+import repository.StudentXMLRepository;
+import repository.TemaXMLRepository;
+import validation.NotaValidator;
+import validation.StudentValidator;
+import validation.TemaValidator;
+import validation.Validator;
+
+public class Lab4 extends TestCase {
+    protected Validator<Student> studentValidator;
+    protected Validator<Tema> temaValidator;
+    protected Validator<Nota> notaValidator;
+
+    protected StudentXMLRepository fileRepository1;
+    protected TemaXMLRepository fileRepository2;
+    protected NotaXMLRepository fileRepository3;
+
+    protected Service service;
+
+    //assigning the values
+    protected void setUp(){
+        studentValidator = new StudentValidator();
+        temaValidator = new TemaValidator();
+        notaValidator = new NotaValidator();
+        fileRepository1 = new StudentXMLRepository(studentValidator, "studenti.xml");
+        fileRepository2 = new TemaXMLRepository(temaValidator, "teme.xml");
+        fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
+        service = new Service(fileRepository1, fileRepository2, fileRepository3);
+    }
+
+    public void testaddGradeBigBang(){
+        service.saveTema("2", "desc", 10, 1);
+        service.saveStudent("2", "titus", 934);
+        assertEquals(service.saveNota("2", "2", 9.8, 1, "alabalaportocala"), 0);
+    }
+
+    public void testaddStudentBigBang(){
+        assertEquals(service.saveStudent("1", "titus", 934), 0);
+    }
+
+    public void testaddAssignmentBigBang(){
+        assertEquals(service.saveTema("1", "desc", 10, 1), 0);
+    }
+
+    public void testintegrateBigBang(){
+        testaddGradeBigBang();
+        testaddAssignmentBigBang();
+        testaddStudentBigBang();
+    }
+}
--- src/test/java/service/ServiceTest.java
+++ src/test/java/service/ServiceTest.java
@@ -36,11 +36,11 @@ public class ServiceTest extends TestCase{
     }
 
     public void testSaveTemaTrue(){
-        Assert.assertEquals(service.saveTema("3", "descriere", 10, 3), 0);
+        assertEquals(service.saveTema("3", "descriere", 10, 3), 0);
     }
 
     public void testSaveTemaFalse(){
-        Assert.assertEquals(service.saveTema(null, "descriere2", 12, 1), 1);
+        assertEquals(service.saveTema(null, "descriere2", 12, 1), 1);
     }
 
     public void testaddStudentValid(){
@@ -48,39 +48,39 @@ public class ServiceTest extends TestCase{
     }
 
     public void testaddStudentNullId(){
-        Assert.assertEquals(service.saveStudent( null, "titus", 934), 1);
+        assertEquals(service.saveStudent( null, "titus", 934), 1);
     }
 
     public void testaddStudentEmptyId(){
-        Assert.assertEquals(service.saveStudent( "", "titus", 934), 1);
+        assertEquals(service.saveStudent( "", "titus", 934), 1);
     }
 
     public void testaddStudentNullName(){
-        Assert.assertEquals(service.saveStudent( "12", null, 934), 1);
+        assertEquals(service.saveStudent( "12", null, 934), 1);
     }
 
     public void testaddStudentEmptyName(){
-        Assert.assertEquals(service.saveStudent( "12", "", 934), 1);
+        assertEquals(service.saveStudent( "12", "", 934), 1);
     }
 
     public void testaddStudentBigGroup(){
-        Assert.assertEquals(service.saveStudent( "15", "titus", 999), 1);
+        assertEquals(service.saveStudent( "15", "titus", 999), 1);
     }
 
     public void testaddStudentSmallGroup(){
-        Assert.assertEquals(service.saveStudent( "15", "titus", 100), 1);
+        assertEquals(service.saveStudent( "15", "titus", 100), 1);
     }
 
 
     public void testaddStudentBVAGroupSmallValid(){
-        Assert.assertEquals(service.saveStudent( "12", "titus", 111), 0);
+        assertEquals(service.saveStudent( "12", "titus", 111), 0);
     }
 
     public void testaddStudentBVAGroupSmallInvalid(){
-        Assert.assertEquals(service.saveStudent( "15", "titus", 110), 1);
+        assertEquals(service.saveStudent( "15", "titus", 110), 1);
     }
 
     public void testaddStudentBVAGroupBigValid(){
-        Assert.assertEquals(service.saveStudent( "15", "titus", 937), 0);
+        assertEquals(service.saveStudent( "16", "titus", 937), 0);
     }
 }
