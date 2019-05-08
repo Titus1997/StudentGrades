3741706682710a1fe5d3057b575c10daac89112c
Titus1997 <trifon.titus@yahoo.com>
2019-05-08 10:47:26 +0300

    top down integration - changed Lab4

--- src/test/java/service/Lab4.java
+++ src/test/java/service/Lab4.java
@@ -4,6 +4,8 @@ import domain.Nota;
 import domain.Student;
 import domain.Tema;
 import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
 import repository.NotaXMLRepository;
 import repository.StudentXMLRepository;
 import repository.TemaXMLRepository;
@@ -12,7 +14,9 @@ import validation.StudentValidator;
 import validation.TemaValidator;
 import validation.Validator;
 
-public class Lab4 extends TestCase {
+import static junit.framework.TestCase.assertEquals;
+
+public class Lab4 {
     protected Validator<Student> studentValidator;
     protected Validator<Tema> temaValidator;
     protected Validator<Nota> notaValidator;
@@ -24,7 +28,8 @@ public class Lab4 extends TestCase {
     protected Service service;
 
     //assigning the values
-    protected void setUp(){
+    @Before
+    public void setUp(){
         studentValidator = new StudentValidator();
         temaValidator = new TemaValidator();
         notaValidator = new NotaValidator();
@@ -33,7 +38,7 @@ public class Lab4 extends TestCase {
         fileRepository3 = new NotaXMLRepository(notaValidator, "note.xml");
         service = new Service(fileRepository1, fileRepository2, fileRepository3);
     }
-
+/*
     public void testaddGradeBigBang(){
         service.saveTema("2", "desc", 10, 1);
         service.saveStudent("2", "titus", 934);
@@ -52,5 +57,23 @@ public class Lab4 extends TestCase {
         testaddGradeBigBang();
         testaddAssignmentBigBang();
         testaddStudentBigBang();
+    }*/
+
+    @Test
+    public void testAddStudentTopDown(){
+        assertEquals(0, this.service.saveStudent("12", "TTTT", 934));
+    }
+
+    @Test
+    public void testAddAssignmentTopDown(){
+        this.testAddStudentTopDown();
+        assertEquals(0, this.service.saveTema("12", "SSVV", 10, 8));
+    }
+
+    @Test
+    public void testAddGradeTopDown(){
+        this.testAddStudentTopDown();
+        this.testAddAssignmentTopDown();
+        assertEquals(0, this.service.saveNota("12", "12", 9.8, 10, "perfect"));
     }
 }
